dc054a1da7fabeda0f44af2e5c37a8e6c4a5d8d7
Jeff Liu <mangomystery@gmail.com>
2015-01-26 14:42:31 -0800

    fix keys

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+
   def milliseconds(time)
     seconds=(time/1000)
     Time.at(seconds).strftime("%M:%S")
@@ -6,5 +7,47 @@ module ApplicationHelper
 
   def percent(decimal)
     (decimal*100).round
+  end
+
+  def mode(num)
+    if num == 1
+      "B"
+    else
+      "A"
+    end
+  end
+
+  def harmonic_mixing(key)
+    if key.include? 'B'
+      case key.first
+        when '0' then '8B'
+        when '1' then '3B'
+        when '2' then '10B'
+        when '3' then '5B'
+        when '4' then '12B'
+        when '5' then '7B'
+        when '6' then '2B'
+        when '7' then '9B'
+        when '8' then '4B'
+        when '9' then '11B'
+        when '10' then '6B'
+        when '11' then '1B'
+      end
+    else key.include? 'A'
+      case key.first
+        when '0' then '5A'
+        when '1' then '12A'
+        when '2' then '7A'
+        when '3' then '2A'
+        when '4' then '9A'
+        when '5' then '4A'
+        when '6' then '11A'
+        when '7' then '6A'
+        when '8' then '1A'
+        when '9' then '8A'
+        when '10' then '3A'
+        when '11' then '10A'
+      end
+    end
   end
 end

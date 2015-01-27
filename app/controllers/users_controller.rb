022f4626891af8a7faff744e64482a163eb683a5
Jeff Liu <mangomystery@gmail.com>
2015-01-26 21:47:09 -0800

    styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,22 +5,3 @@ class UsersController < ApplicationController
     render 'pages/welcome'
   end
 end
-
-    # spotify_user.country #=> "US"
-    # spotify_user.email   #=> "example@email.com"
-
-    # # Create playlist in user's Spotify account
-    # playlist = spotify_user.create_playlist!('my-awesome-playlist')
-
-    # # Add tracks to a playlist in user's Spotify account
-    # tracks = RSpotify::Track.search('Know')
-    # playlist.add_tracks!(tracks)
-    # playlist.tracks.first.name #=> "Somebody That I Used To Know"
-
-    # # Access and modify user's music library
-    # spotify_user.save_tracks!(tracks)
-    # spotify_user.saved_tracks.size #=> 20
-    # spotify_user.remove_tracks!(tracks)
-
-    # # p 'got user : '
-    # # p spotify_user

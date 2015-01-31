8a2f07a942f0b98e0d004a2fcad8e3f327fa146c
Jeff Liu <mangomystery@gmail.com>
2015-01-30 18:27:16 -0800

    changes + javasciprt/ajax attempt

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,9 +7,8 @@ class PagesController < ApplicationController
 
   def playlist
     @playlist = RSpotify::Playlist.find(params[:owner], params[:id])
-    @track_info = @playlist.tracks.map { |track| [track.id, track.name, track.artists.first.name] }
-    @track_info.delete_if { |x| x.include? nil }
-    spotify_track_ids = @track_info.map { |info| "spotify:track:" + info[0] }
-    @profile = spotify_track_ids.map { |track_id| Echowrap.song_profile(:track_id => track_id, :bucket => ['audio_summary', 'song_hotttnesss'])}
+    @track_info = @playlist.tracks.map { |track| [track.uri, track.name, track.artists.first.name] }
+    @track_info.delete_if { |x| x.include? "spotify:track:null" }
+    @profile = @track_info.map { |track| Echowrap.song_profile(:track_id => track[0], :bucket => ['audio_summary', 'song_hotttnesss'])}
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def spotify
     spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
     session[:user_id] = spotify_user.id
+    @playlists = current_user.playlists.select { |playlist| playlist.owner.id == current_user.id }
     render 'pages/welcome'
   end
 end

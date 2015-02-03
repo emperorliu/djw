f36a4284b23801dab9c996cac01fc8d5629444e1
Jeff Liu <mangomystery@gmail.com>
2015-02-03 14:42:37 -0800

    hm

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,7 @@ class PagesController < ApplicationController
 
   def playlist
     @playlist = RSpotify::Playlist.find(params[:owner], params[:id])
-    @track_info = @playlist.tracks.map { |track| [track.uri, track.name, track.artists.first.name] }
+    @track_info = @playlist.tracks.map { |track| [track.uri, track.name, track.artists.first.name, track.preview_url] }
     @track_info.delete_if { |x| x.include? "spotify:track:null" }
     @profile = @track_info.map { |track| Echowrap.song_profile(:track_id => track[0], :bucket => ['audio_summary', 'song_hotttnesss'])}
   end
